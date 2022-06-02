9ed181ae0638b2d56563c3c811acfabb8377712e
Nadine Rodriguez <inadinerod@gmail.com>
2022-06-01 20:20:56 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var num = "12";
+var num1 = 12;
+
+if (num === num1) {
+  console.log("Yes, this is true");
+} else if (num == num1) {
+  console.log("The value of these two variables are the same.");
+} else {
+  console.log("Not sure what happened here, but something went wrong.");
+}
